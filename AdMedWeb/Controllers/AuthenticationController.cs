95742f5b7cb4f4378949869bc4c5bb2b12befb9e
Karl Dicks <17667327@vcconnect.co.za>
2020-10-22 23:00:42 +0200

    Design Updated

---
+++ AdMedWeb/Controllers/AuthenticationController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AdMedWeb.Models;
+using AdMedWeb.Repository.IRepository;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AdMedWeb.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+
+        private readonly IAccountRepository _accRepo;
+
+        public AuthenticationController(IAccountRepository accRepo)
+        {
+
+            _accRepo = accRepo;
+
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            User obj = new User();
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(User obj)
+        {
+            User objUser = await _accRepo.LoginAsync(SD.AccountAPIPath + "authenticate/", obj);
+            if (objUser.Token == null)
+            {
+                TempData["Error"] = "Incorrect Username / Password Combination.";
+                return View();
+            }
+
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+            identity.AddClaim(new Claim(ClaimTypes.Name, objUser.Username));
+            identity.AddClaim(new Claim(ClaimTypes.Role, objUser.Role));
+            var principal = new ClaimsPrincipal(identity);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+            HttpContext.Session.SetString("JWToken", objUser.Token);
+            TempData["alert"] = "Welcome " + objUser.Username;
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(User obj)
+        {
+            bool result = await _accRepo.RegisterAsync(SD.AccountAPIPath + "register/", obj);
+            if (result == false)
+            {
+                TempData["Error"] = "Account already in use!";
+                return View();
+            }
+            TempData["alert"] = "Registration Successful";
+            return RedirectToAction("Login");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            HttpContext.Session.SetString("JWToken", "");
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
--- AdMedWeb/Controllers/HomeController.cs
+++ AdMedWeb/Controllers/HomeController.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AdMedWeb.Models;
-using AdMedWeb.Models.ViewModels;
 using AdMedWeb.Repository.IRepository;
 using Microsoft.AspNetCore.Identity.UI.Services;
 
@@ -92,62 +86,6 @@ namespace AdMedWeb.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        [HttpGet]
-        public IActionResult Login()
-        {
-            User obj = new User();
-            return View(obj);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(User obj)
-        {
-            User objUser = await _accRepo.LoginAsync(SD.AccountAPIPath + "authenticate/", obj);
-            if (objUser.Token == null)
-            {
-                TempData["Error"] = "Incorrect Username / Password Combination.";
-                return View();
-            }
-
-            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
-            identity.AddClaim(new Claim(ClaimTypes.Name, objUser.Username));
-            identity.AddClaim(new Claim(ClaimTypes.Role, objUser.Role));
-            var principal = new ClaimsPrincipal(identity);
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
-
-            HttpContext.Session.SetString("JWToken", objUser.Token);
-            TempData["alert"] = "Welcome " + objUser.Username;
-            return RedirectToAction("Index");
-        }
-
-        [HttpGet]
-        public IActionResult Register()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Register(User obj)
-        {
-            bool result = await _accRepo.RegisterAsync(SD.AccountAPIPath + "register/", obj);
-            if (result == false)
-            {
-                TempData["Error"] = "Account already in use!";
-                return View();
-            }
-            TempData["alert"] = "Registration Successful";
-            return RedirectToAction("Login");
-        }
-
-        public async Task<IActionResult> Logout()
-        {
-            await HttpContext.SignOutAsync();
-            HttpContext.Session.SetString("JWToken", "");
-            return RedirectToAction("Index");
         }
 
         [HttpGet]
