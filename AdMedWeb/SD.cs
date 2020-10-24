416d3a8853f1507af1689b3d02cf17588e9492b3
Karl Dicks <17667327@vcconnect.co.za>
2020-10-25 01:17:19 +0200

    Update connection string to live API

--- AdMedWeb/SD.cs
+++ AdMedWeb/SD.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace AdMedWeb
+﻿namespace AdMedWeb
 {
     public static class SD
     {
 
-        //public static string APIBaseUrl = "https://admedapi.azurewebsites.net/";
-        public static string APIBaseUrl = "https://localhost:44399/";
+        public static string APIBaseUrl = "https://admedapi.azurewebsites.net/";
+        //public static string APIBaseUrl = "https://localhost:44399/";
         public static string ApplicationAPIPath = APIBaseUrl + "api/v1/applications/";
         public static string ResidentAPIPath = APIBaseUrl + "api/v1/residents/";
         public static string MedicationAPIPath = APIBaseUrl + "api/v1/medications/";
